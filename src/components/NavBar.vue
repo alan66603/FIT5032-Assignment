7d6127b58cd49bd98c5860becee381c3c8c32662
alan <alan66603@gmail.com>
2026-08-23 19:27:46 +1000

    feat: implemented navbar and toggler button for mobile version

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-white border-bottom py-3">
+        <div class="contatiner">
+            <router-link class="navbar-brand fw-bold" to="/">GREENROOTS</router-link>
+
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#mainNav"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="mainNav">
+                <ul class="navbar-nav ms-auto gap-lg-3">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/login">Log In</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
