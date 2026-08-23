eef6d28b2d00a59cd07e10d8ffb467fb081debbd
alan <alan66603@gmail.com>
2026-08-23 20:36:44 +1000

    feat: implement grant search bar (ref & computed)

--- src/views/GrantsView.vue
+++ src/views/GrantsView.vue
@@ -1,11 +1,28 @@
 <script setup>
+import { ref, computed } from 'vue';
 import { grants } from '@/data/grants';
+
+const searchTerm = ref('')
+
+const filterGrants = computed(() => {
+    return grants.filter((grant) => 
+    grant.project.toLowerCase().includes(searchTerm.value.toLowerCase())
+    )
+})
 </script>
 
 <template>
     <div class="container py-5">
         <h1>Community Greening Grants</h1>
-        <p>We have {{ grants.length }} grants.</p>
+        <input
+            v-model="searchTerm"
+            type="text"
+            class="form-control mb-3"
+            style="max-width: 400px"
+            placeholder="Search by project name"
+        />
+
+        <p>We have {{ filterGrants.length }} grants.</p>
 
         <table class="table">
             <thead>
@@ -18,7 +35,7 @@ import { grants } from '@/data/grants';
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="grant in grants" :key="grant.id">
+                <tr v-for="grant in filterGrants" :key="grant.id">
                     <td>{{ grant.project }}</td>
                     <td>{{ grant.suburb }}</td>
                     <td>${{ grant.amount.toLocaleString() }}</td>
