84193825ba84f2472d4a0823c33d3e8a63e85f5c
alan <alan66603@gmail.com>
2026-08-25 01:01:43 +1000

    feat: link navbar with authentication status

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,9 @@
 <script setup>
+import { isAuthenticated } from '@/auth';
+
+function handleLogout() {
+    isAuthenticated.value = false
+}
 </script>
 
 <template>
@@ -26,8 +31,11 @@
                     <li class="nav-item">
                         <router-link class="nav-link" to="/about">About</router-link>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item" v-if="!isAuthenticated">
                         <router-link class="nav-link" to="/login">Log In</router-link>
+                    </li>
+                    <li class="nav-item" v-else>
+                        <router-link class="btn btn-outline-dark btn-sm" @click="handleLogout">Log Out</router-link>
                     </li>
                 </ul>
             </div>
